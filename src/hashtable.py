ba9807caadb128998cabf120bfc40efcd4159d09
Penny Tague <pmtague9@gmail.com>
2020-03-10 15:03:11 -0700

    Collisions accounted for

--- src/hashtable.py
+++ src/hashtable.py
@@ -20,6 +20,7 @@ class HashTable:
 	def __init__(self, capacity):
 		self.capacity = capacity  # Number of buckets in the hash table
 		self.storage = [None] * capacity
+		self.count = 0
 
 	def _hash(self, key):
 		'''
@@ -54,12 +55,25 @@ class HashTable:
 		Fill this in.
 		'''
 		# Hash key
-		self._hash_mod(key)
+		converted_key = self._hash_mod(key)
 		# Hashtable is full
-
-		# Collisions
-		
-		self.storage[hashed_key] = value
+		if self.count == self.capacity:
+			self.resize()
+			self.insert(key, value)
+		# No value in hashtable slot
+		if self.storage[converted_key] == None:
+			self.storage[converted_key] = value
+		else:
+			# If key already exists, store the old value, add the new value to the table,
+			# and return the old value
+			if self.storage[converted_key] == key:
+				old_value = self.storage[converted_key]
+				self.storage[converted_key] = value
+				return old_value
+			else:
+				# Handle collision here
+				new_list = LinkedPair(converted_key, value)
+				
 
 	def remove(self, key):
 		'''
@@ -69,7 +83,12 @@ class HashTable:
 
 		Fill this in.
 		'''
-		pass
+		if not key:
+			print("Error: I can't delete something that doesn't exist.")
+		else:
+			converted_key = self._hash_mod(key)
+			self.storage[converted_key] = None
+			key = None
 
 	def retrieve(self, key):
 		'''
@@ -79,7 +98,11 @@ class HashTable:
 
 		Fill this in.
 		'''
-		pass
+		if not key:
+			return None
+		else:
+			converted_key = self._hash_mod(key)
+			return self.storage[converted_key]
 
 	def resize(self):
 		'''
@@ -88,7 +111,10 @@ class HashTable:
 
 		Fill this in.
 		'''
-		pass
+		self.capacity *= 2
+
+		for key, value in range(0, self.capacity):
+			self.storage.insert(key, value)
 
 
 if __name__ == "__main__":
